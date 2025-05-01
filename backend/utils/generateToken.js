af40adfbe4639c6e01644f56347e11bc6e87bf77
mdmansooralam <mdmansooralam@outlook.in>
2025-05-01 11:04:49 +0530

    cookies allowed for cross site

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,7 +4,11 @@ import jwt from 'jsonwebtoken'
 const generateToken = (userId, res)=>{
     try {
         const token = jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn:'7d'})
-        res.cookie('jwt', token, { maxAge: 7 * 24 * 60 * 60 * 1000, httpOnly: true})
+        res.cookie('jwt', token, { 
+            maxAge: 7 * 24 * 60 * 60 * 1000, 
+            httpOnly: true,
+            sameSite:'None'
+        })
         return token
     } catch (error) {
         console.log('Error in generateToken : ', error)
